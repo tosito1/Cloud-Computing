0436b7a682af6abebd3adf4a84029c09ea897345
Antonio José Muriel Gálvez <antoniojse2001@gmail.com>
2024-10-29 12:14:56 +0100

    Eliminacion de la lógica para controlar Feria y San Juan

--- src/app.py
+++ src/app.py
@@ -4,8 +4,6 @@ from controllers.votaciones import votaciones_bp
 from controllers.notificaciones import notificaciones_bp
 from controllers.dinero import dinero_bp
 from controllers.usuarios import usuarios_bp
-from controllers.feria import feria_bp  
-from controllers.san_juan import san_juan_bp 
 from controllers.index import index_bp
 
 app = Flask(__name__)
@@ -18,8 +16,6 @@ app.register_blueprint(votaciones_bp)
 app.register_blueprint(notificaciones_bp)
 app.register_blueprint(dinero_bp)
 app.register_blueprint(usuarios_bp)
-app.register_blueprint(feria_bp) 
-app.register_blueprint(san_juan_bp) 
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/controllers/feria.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from controllers.auth import login_requerido
-
-feria_bp = Blueprint('feria', __name__)
-
-@feria_bp.route('/feria')
-@login_requerido
-def feria():
-    return render_template('feria.html')  # Página de Feria
